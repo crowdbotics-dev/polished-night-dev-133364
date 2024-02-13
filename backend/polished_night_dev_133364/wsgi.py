30f9ecfd019bdbef5de0a33e63ca169a71015a0c
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 18:17:00 +0000

    Initial commit

---
+++ backend/polished_night_dev_133364/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for polished_night_dev_133364 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "polished_night_dev_133364.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-night-dev-133364",
+  url: "https://polished-night-dev-133364.botics.co",
+  api: "https://polished-night-dev-133364.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
